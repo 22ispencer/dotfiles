cf003ffa217936497b220b55cdb0dd0fb493f035
Isaac Spencer <22ispencer@gmail.com>
2024-06-24 09:46:07 -0500

    finished for now

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -23,8 +23,11 @@ vim.opt.signcolumn = "yes"
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.wrap = false
 vim.opt.clipboard = "unnamed,unnamedplus"
-vim.g.mapleader = "<Space>"
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<Leader>cl", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- Lazy plugins
 require("lazy").setup({
@@ -34,9 +37,58 @@ require("lazy").setup({
 		config = function()
 			local modules = {
 				"ai",
+				{
+					"clue",
+					opts = {
+						triggers = {
+							-- Leader triggers
+							{ mode = "n", keys = "<Leader>" },
+							{ mode = "x", keys = "<Leader>" },
+
+							-- Built-in completion
+							{ mode = "i", keys = "<C-x>" },
+
+							-- `g` key
+							{ mode = "n", keys = "g" },
+							{ mode = "x", keys = "g" },
+
+							-- Marks
+							{ mode = "n", keys = "'" },
+							{ mode = "n", keys = "`" },
+							{ mode = "x", keys = "'" },
+							{ mode = "x", keys = "`" },
+
+							-- Registers
+							{ mode = "n", keys = '"' },
+							{ mode = "x", keys = '"' },
+							{ mode = "i", keys = "<C-r>" },
+							{ mode = "c", keys = "<C-r>" },
+
+							-- Window commands
+							{ mode = "n", keys = "<C-w>" },
+
+							-- `z` key
+							{ mode = "n", keys = "z" },
+							{ mode = "x", keys = "z" },
+						},
+
+						clues = {
+							-- Enhance this by adding descriptions for <Leader> mapping groups
+							require("mini.clue").gen_clues.builtin_completion(),
+							require("mini.clue").gen_clues.g(),
+							require("mini.clue").gen_clues.marks(),
+							require("mini.clue").gen_clues.registers(),
+							require("mini.clue").gen_clues.windows(),
+							require("mini.clue").gen_clues.z(),
+							{ mode = "n", keys = "<Leader>f", desc = "+Find" },
+							{ mode = "n", keys = "<Leader>c", desc = "+Code" },
+						},
+					},
+				},
 				"files",
 				"indentscope",
 				"jump",
+				"jump2d",
 				"notify",
 				"pairs",
 				"starter",
@@ -47,7 +99,15 @@ require("lazy").setup({
 			}
 
 			for _, module in ipairs(modules) do
-				require("mini." .. module).setup({})
+				local mod_name = nil
+				local opts = {}
+				if type(module) == "table" then
+					mod_name = module[1]
+					opts = module["opts"]
+				else
+					mod_name = module
+				end
+				require("mini." .. mod_name).setup(opts)
 			end
 		end,
 	},
@@ -69,20 +129,27 @@ require("lazy").setup({
 			package_pending = "➜",
 			package_uninstalled = "✗",
 		},
+		keys = {
+			{ "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
+		},
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
 			"neovim/nvim-lspconfig",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			require("mason-lspconfig").setup({})
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			require("mason-lspconfig").setup_handlers({
 				function(server_name)
-					require("lspconfig")[server_name].setup({})
-
-
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
 				end,
 				["lua_ls"] = function()
 					require("lspconfig").lua_ls.setup({
@@ -115,21 +182,19 @@ require("lazy").setup({
 								},
 							})
 						end,
-                        settings = {
-                            Lua = {}
-                        }
+						settings = {
+							Lua = {},
+						},
 					})
 				end,
 			})
-            vim.lsp.handlers["textDocument/diagnostic"] = vim.lsp.with(
-                vim.lsp.diagnostic.on_diagnostic, {
-                    underline = true,
-                    virtual_text = {
-                        spacing = 4,
-                    },
-                    signs = false
-                }
-            )
+			vim.lsp.handlers["textDocument/diagnostic"] = vim.lsp.with(vim.lsp.diagnostic.on_diagnostic, {
+				underline = true,
+				virtual_text = {
+					spacing = 4,
+				},
+				signs = false,
+			})
 		end,
 	},
 	{
@@ -148,5 +213,181 @@ require("lazy").setup({
 		opts = {
 			handlers = {},
 		},
+		keys = {
+			{ "<leader>cf", "<cmd>lua vim.lsp.buf.format()<cr>", desc = "Format buffer" },
+			{ "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename symbol" },
+		},
+	},
+	{
+
+		"saadparwaiz1/cmp_luasnip",
+		dependencies = { "L3MON4D3/LuaSnip" },
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							if luasnip.expandable() then
+								luasnip.expand()
+							else
+								cmp.confirm({
+									select = true,
+								})
+							end
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"elixir",
+					"heex",
+					"javascript",
+					"html",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-fzf-native.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			-- This is your opts table
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+
+						-- pseudo code / specification for writing custom displays, like the one
+						-- for "codeactions"
+						-- specific_opts = {
+						--   [kind] = {
+						--     make_indexed = function(items) -> indexed_items, width,
+						--     make_displayer = function(widths) -> displayer
+						--     make_display = function(displayer) -> function(e)
+						--     make_ordinal = function(e) -> string
+						--   },
+						--   -- for example to disable the custom builtin "codeactions" display
+						--      do the following
+						--   codeactions = false,
+						-- }
+					},
+				},
+			})
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffer" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+		},
 	},
 })
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	buffer = vim.fn.bufnr(),
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 3000 })
+	end,
+})
+
+-- Keybinds
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
