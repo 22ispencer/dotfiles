a5ee11859335a140086ab74b92d90675ec4258f2
Isaac Spencer <22ispencer@gmail.com>
2024-07-16 09:19:26 -0500

    format on save

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -98,6 +98,24 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
+        init = function()
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            require("null-ls").setup({
+                -- you can reuse a shared lspconfig on_attach callback here
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({ async = false })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
     },
     {
         "jay-babu/mason-null-ls.nvim",
