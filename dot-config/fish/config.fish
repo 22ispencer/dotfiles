c391cb0c81281d090980a9111cdf7b8e1c19d725
Isaac Spencer <22ispencer@gmail.com>
2024-08-04 10:19:46 -0500

    reordered config

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,23 +1,26 @@
-if type -q neofetch;
-    neofetch
+if status is-interactive;
+    # Commands to run in interactive sessions can go here
+    if type -q neofetch;
+        neofetch
+    end
+
+    fish_vi_key_bindings
+    fish_vi_cursor
+
+    set fish_greeting
+    starship init fish | source
 end
+
 # Fundle (package manager)
-if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+if not functions -q fundle;
+    eval (curl -sfL https://git.io/fundle-install);
+end
 fundle plugin nickeb96/puffer-fish
 fundle plugin jorgebucaran/autopair.fish
 fundle plugin jethrokuan/z
 fundle plugin gazorby/fish-abbreviation-tips
 fundle plugin PatrickF1/fzf.fish
 fundle init
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set fish_greeting
-
-fish_vi_key_bindings
-fish_vi_cursor
 
 if test (uname) = Darwin;
 	fish_add_path /opt/homebrew/bin
@@ -26,4 +29,3 @@ end
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.cargo/bin
 
-starship init fish | source
