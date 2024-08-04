3a3e58e98fbee56bbd650d7cf02accc8218a0ef8
Isaac Spencer <22ispencer@gmail.com>
2024-08-03 20:20:28 -0500

    fundle and fish plugins

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,3 +1,14 @@
+if type -q neofetch;
+    neofetch
+end
+# Fundle (package manager)
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+fundle plugin nickeb96/puffer-fish
+fundle plugin jorgebucaran/autopair.fish
+fundle plugin jethrokuan/z
+fundle plugin gazorby/fish-abbreviation-tips
+fundle init
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -6,5 +17,12 @@ set fish_greeting
 
 fish_vi_key_bindings
 fish_vi_cursor
+
+if test (uname) = Darwin;
+	fish_add_path /opt/homebrew/bin
+end
+
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.cargo/bin
 
 starship init fish | source
