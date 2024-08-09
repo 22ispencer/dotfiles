a991c1f33ea3a21c0794d46d156508db9afb5cf9
Isaac Spencer <22ispencer@gmail.com>
2024-08-09 11:19:35 -0500

    built in wezterm colors

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -3,8 +3,40 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("FiraCode Nerd Font")
-config.color_scheme_dirs = { "~/dotfiles/dot-config/wezterm/colors/" }
 
-config.color_scheme = "Sonokai (Gogh)"
+config.colors = {
+	foreground = "#e3e1e4",
+	background = "#2d2a2e",
+
+	cursor_bg = "#e3e1e4",
+	cursor_fg = "#2d2a2e",
+	cursor_border = "#e3e1e4",
+
+	selection_fg = "#e3e1e4",
+	selection_bd = "#354157",
+
+	ansi = {
+		"#2d2a2e",
+		"#f85e84",
+		"#9ecd6f",
+		"#e5c463",
+		"#ef9062",
+		"#ab9df2",
+		"#7accd7",
+		"#e3e1e4",
+	},
+	brights = {
+		"#848089",
+		"#f85e84",
+		"#9ecd6f",
+		"#e5c463",
+		"#ef9062",
+		"#ab9df2",
+		"#7accd7",
+		"#e3e1e4",
+	},
+
+	compose_cursor = "#ef9062",
+}
 
 return config
