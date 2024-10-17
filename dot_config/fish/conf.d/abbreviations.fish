70a1b5568b067fb9c97ae68fd643a98adfd64e05
Isaac Spencer <22ispencer@gmail.com>
2024-10-17 10:39:50 -0500

    new abbr

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -4,3 +4,6 @@ abbr -a --set-cursor='%' -- gcm 'git commit -m "%"'
 abbr -a --position anywhere -- fcfg /Users/isaacspencer/.config/fish/config.fish
 abbr -a -- tmn tmuxinator
 abbr -a --position anywhere -- zyp zypper
+abbr -a -- gco 'git checkout'
+abbr -a -- gdr 'git stash -u && git stash drop'
+abbr -a -- cz chezmoi
