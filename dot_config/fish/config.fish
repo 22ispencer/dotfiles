748996863cebfefc6aa9a35e96afcdf2cbf54069
Isaac Spencer <22ispencer@gmail.com>
2024-11-29 11:40:43 -0600

    nvim completion and kanata caps-word and fish vars

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -37,3 +37,17 @@ end
 
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.cargo/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/isaacspencer/miniforge3/bin/conda
+    eval /Users/isaacspencer/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/isaacspencer/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/Users/isaacspencer/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/isaacspencer/miniforge3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
